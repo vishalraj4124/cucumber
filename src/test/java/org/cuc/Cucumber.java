5380b89bb1121e985483f530922cf9b2e844e76e
vishalraj4124 <vishalraj4124@gmail.com>
2019-08-28 19:32:17 +0530

    uplod

--- src/test/java/org/cuc/Cucumber.java
+++ src/test/java/org/cuc/Cucumber.java
@@ -24,7 +24,7 @@ public void user_should_launch_browser() {
 
 @Given("user click add customer link")
 public void user_click_add_customer_link() {
-	driver.findElement(By.xpath("(//a[text()='Add Customer'])[1]")).click();    
+	driver.findElement(By.xpath("//a[text()='Add Tariff Plan']")).click();
 }
 /*@When("user will provide vaild details")
 public void user_will_provide_vaild_details() {
@@ -62,7 +62,7 @@ public void user_will_provide_vaild_details(io.cucumber.datatable.DataTable data
 	driver.findElement(By.xpath("//input[@name='submit']")).click();        
 }*/
 
-@When("user will provide vaild details")
+/*@When("user will provide vaild details")
 public void user_will_provide_vaild_details(io.cucumber.datatable.DataTable dataTable) {
 Map<String, String> asMap = dataTable.asMap(String.class, String.class);
 driver.findElement(By.xpath("//label[text()='Done']")).click();  
@@ -73,12 +73,45 @@ driver.findElement(By.xpath("//textarea[@name='addr']")).sendKeys(asMap.get("add
 driver.findElement(By.id("telephoneno")).sendKeys(asMap.get("phone"));
 driver.findElement(By.xpath("//input[@name='submit']")).click();
 
+}*/
+
+/*@Then("to verified the customer id is displayed")
+public void to_verified_the_customer_id_is_displayed() {
+	Assert.assertTrue(driver.findElement(By.xpath("(//td[@align='center'])[2]")).isDisplayed());
+}*/
+
+/*@When("user will provide vaild details")
+public void user_will_provide_vaild_details(io.cucumber.datatable.DataTable dataTable) {
+	List<List<String>> twodlist = dataTable.asLists(String.class);
+    System.out.println(twodlist);
+   // driver.findElement(By.xpath("(//(a[@href='addtariffplans.php'])[1]")).click();
+    driver.findElement(By.id("rental1")).sendKeys(twodlist.get(0).get(0));
+    driver.findElement(By.id("local_minutes")).sendKeys(twodlist.get(0).get(1));
+    driver.findElement(By.id("inter_minutes")).sendKeys(twodlist.get(0).get(2));
+    driver.findElement(By.id("sms_pack")).sendKeys(twodlist.get(1).get(0));
+    driver.findElement(By.id("minutes_charges")).sendKeys(twodlist.get(1).get(1));
+    driver.findElement(By.id("inter_charges")).sendKeys(twodlist.get(2).get(2));
+    driver.findElement(By.id("sms_charges")).sendKeys(twodlist.get(2).get(1));
+    driver.findElement(By.xpath("//input[@type='submit']")).click();   
+}*/
+
+@When("user will provide vaild details")
+public void user_will_provide_vaild_details(io.cucumber.datatable.DataTable dataTable) {
+List<Map<String, String>> twodmap = dataTable.asMaps(String.class,String.class);
+System.out.println(twodmap);
+driver.findElement(By.id("rental1")).sendKeys(twodmap.get(0).get("rental"));
+driver.findElement(By.id("local_minutes")).sendKeys(twodmap.get(1).get("minutes"));
+driver.findElement(By.id("inter_minutes")).sendKeys(twodmap.get(2).get("international"));
+driver.findElement(By.id("sms_pack")).sendKeys(twodmap.get(2).get("smspack"));
+driver.findElement(By.id("minutes_charges")).sendKeys(twodmap.get(0).get("localperminutes"));
+driver.findElement(By.id("inter_charges")).sendKeys(twodmap.get(1).get("internationalminutes"));
+driver.findElement(By.id("sms_charges")).sendKeys(twodmap.get(0).get("smsper"));
+driver.findElement(By.xpath("//input[@type='submit']")).click();
 }
 
 @Then("to verified the customer id is displayed")
 public void to_verified_the_customer_id_is_displayed() {
-	Assert.assertTrue(driver.findElement(By.xpath("(//td[@align='center'])[2]")).isDisplayed());
+	Assert.assertTrue(driver.findElement(By.xpath("//h2[@style='text-align: center;font-weight: 700;font-size:28px;']")).isDisplayed());
 }
-
 
 }
---
+++ src/test/resources/features/twodmap.feature
@@ -0,0 +1,14 @@
+@tag
+Feature: Title of your feature
+  I want to use this template for my feature file
+
+  @tag1
+  Scenario: Title of your scenario
+    Given user should launch browser
+    And user click add customer link
+    When user will provide vaild details
+      | rental | minutes | international | smspack | localperminutes | internationalminutes | smsper | 
+      |   1000 |    1001 |          1002 |    1003 |            1004 |                 1005 |   1006 | 
+      |   2000 |    2001 |          2002 |    2003 |            2004 |                 2005 |   2006 | 
+      |   3000 |    3001 |          3002 |    3003 |            3004 |                 3005 |   3006 | 
+    Then to verified the customer id is displayed
